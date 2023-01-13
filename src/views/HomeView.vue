7a3901a2959ca1e50408077d893dedfb6e150427
Maj Drab <maj.drab@gmail.com>
2023-01-13 12:03:34 +0100

    feat: add slider with dots and pointers, and the blue box with all content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,6 +11,7 @@
     </MajTitle>
     <MajContainer vertical class="home" centerX>
         <div class="home__desc"><p>We’re a dynamic group enjoying healthy growth. As a fast-growing group, it regularly needs to add a member to its workforce. As a result there are jobs available regularly, both to full-time employees and to students seeking internships. We offer a unique glimpse into the world of development, marketing, technology, IT, and servers.</p></div>
+        <div class="home__desc"><p class="home__mid-titles">vacancies</p></div>
         <MajContainer class="home__job-overview">
             <section class="home__job-overview-left">
                 <router-link 
@@ -77,7 +78,7 @@
                 <p class="home__job-overview-right-value">12.12.2022 (4 days left)</p>
             </MajContainer> 
         </MajContainer>
-        <MajContainer class="home__job-overview">
+        <MajContainer class="home__job-overview home__job-overview--last">
             <section class="home__job-overview-left">
                 <router-link 
                     title="WordPress developer"
@@ -110,6 +111,45 @@
                 <p class="home__job-overview-right-value">12.12.2022 (4 days left)</p>
             </MajContainer> 
         </MajContainer>
+        <div class="home__desc"><p class="home__mid-titles">faces of humanfrog</p></div>
+        <div class="home__slider">
+            <p class="home__slider-text">To me, Zumo has it all: individualism and togetherness, passion, a place where creativity is listened to and makes an impact. We’re all on the same adventure and seeing the direct result of what we do is the best thing ever.</p>
+            <MajContainer class="home__slider-who">
+                <div></div>
+                <p><span>Amelie Arras</span><br>Marketing Director<br>Zumo Enterprise</p>
+            </MajContainer>
+            
+            <img class="home__slider-image" src="../assets/images/Amelie Arras - Marketing Director 5.png" alt="amelie_image">
+            <svg class="home__slider-apostrophe" viewBox="0 0 617 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M43.4113 22.2989C43.4113 17.1389 39.8113 13.6589 34.6513 13.6589C33.3313 13.6589 32.2513 13.8989 31.2913 14.2589C32.6113 7.53887 36.9313 4.17887 42.8113 1.17886L42.4513 0.338867C32.4913 3.09887 23.2513 11.4989 23.2513 20.0189C23.2513 27.5789 27.2113 31.4189 33.8113 31.4189C39.2113 31.4189 43.4113 27.6989 43.4113 22.2989ZM20.7313 22.2989C20.7313 17.1389 17.2513 13.6589 12.0913 13.6589C10.7713 13.6589 9.69129 13.8989 8.73129 14.2589C9.69129 7.65887 14.3713 4.17887 20.1313 1.17886L19.8913 0.338867C9.93129 3.09887 0.571289 11.4989 0.571289 20.0189C0.571289 27.5789 4.65129 31.4189 11.1313 31.4189C16.5313 31.4189 20.7313 27.6989 20.7313 22.2989Z" fill="black"/>
+                <path d="M573.948 9.45895C573.948 14.619 577.548 18.0989 582.708 18.0989C584.028 18.0989 585.108 17.859 586.068 17.4989C584.748 24.2189 580.428 27.5789 574.548 30.579L574.908 31.4189C584.868 28.6589 594.108 20.2589 594.108 11.739C594.108 4.17895 590.148 0.338946 583.548 0.338946C578.148 0.338947 573.948 4.05895 573.948 9.45895ZM596.628 9.45895C596.628 14.6189 600.108 18.0989 605.268 18.0989C606.588 18.0989 607.668 17.8589 608.628 17.4989C607.668 24.0989 602.988 27.5789 597.228 30.579L597.468 31.4189C607.428 28.6589 616.788 20.2589 616.788 11.7389C616.788 4.17895 612.708 0.338944 606.228 0.338944C600.828 0.338945 596.628 4.05895 596.628 9.45895Z" fill="black"/>
+            </svg>
+            <svg class="home__slider-pointers" viewBox="0 0 1105 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1097.14 1.60449L1102.58 6.82715L1097.14 12.0498L1098.18 13.1162L1104.74 6.82715L1098.18 0.538085L1097.14 1.60449ZM1084.92 7.56543L1084.92 6.08887L1086.4 6.08887L1086.4 7.56543L1084.92 7.56543ZM1087.9 7.56543L1087.9 6.08887L1089.4 6.08887L1089.4 7.56543L1087.9 7.56543ZM1090.91 7.56543L1090.91 6.08887L1092.41 6.08887L1092.41 7.56543L1090.91 7.56543ZM1093.92 7.56543L1093.92 6.08887L1095.42 6.08887L1095.42 7.56543L1093.92 7.56543ZM1096.9 7.56543L1096.9 6.08887L1098.4 6.08887L1098.4 7.56543L1096.9 7.56543ZM1099.9 7.56543L1099.9 6.08887L1101.41 6.08887L1101.41 7.56543L1099.9 7.56543Z" fill="#BECDE2"/>
+                <path d="M8.08008 12.0498L2.63867 6.82715L8.08008 1.60449L7.04102 0.538085L0.478515 6.82715L7.04101 13.1162L8.08008 12.0498ZM20.3027 6.08887L20.3027 7.56543L18.8262 7.56543L18.8262 6.08887L20.3027 6.08887ZM17.3223 6.08887L17.3223 7.56543L15.8184 7.56543L15.8184 6.08887L17.3223 6.08887ZM14.3145 6.08887L14.3145 7.56543L12.8105 7.56543L12.8105 6.08887L14.3145 6.08887ZM11.3066 6.08887L11.3066 7.56543L9.80273 7.56543L9.80273 6.08887L11.3066 6.08887ZM8.32617 6.08887L8.32617 7.56543L6.82226 7.56543L6.82227 6.08887L8.32617 6.08887ZM5.31836 6.08887L5.31836 7.56543L3.81445 7.56543L3.81445 6.08887L5.31836 6.08887Z" fill="#BECDE2"/>
+            </svg>
+            <svg class="home__slider-circles" width="79" height="9" viewBox="0 0 79 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="4.96484" cy="4.52051" r="4" fill="#6C6C6C"/>
+                <circle cx="22.4648" cy="4.52051" r="4" fill="white"/>
+                <circle cx="39.9648" cy="4.52051" r="4" fill="#6C6C6C"/>
+                <circle cx="57.4648" cy="4.52051" r="4" fill="#6C6C6C"/>
+                <circle cx="74.9648" cy="4.52051" r="4" fill="#6C6C6C"/>
+            </svg>
+
+        </div>
+        <MajContainer centerY class="home__bluebox">
+            <div class="home__bluebox-bg"></div>
+            <MajContainer class="home__bluebox-container">
+                <MajContainer vertical class="home__bluebox-container-2">
+                    <MajTitle is="h1">
+                        No Vacancy fits You? We Might Need you Anyway.
+                    </MajTitle>
+                    <div class="home__bluebox-divider"></div>
+                    <p>Lorem ipsum dolor sit amet consectetur. Viverra dictumst diam placerat faucibus tempus dui feugiat volutpat. Lorem ipsum dolor sit amet consectetur. Viverra dictumst diam placerat faucibus tempus dui feugiat volutpat.</p>
+                    <button class="home__bluebox-button"><span>GET IN TOUCH</span></button>
+                </MajContainer>
+            </MajContainer>
+        </MajContainer> 
     </MajContainer>
 </template>
 
@@ -123,7 +163,6 @@ export default {
 <style lang="scss">
 .home {
   width: 100vw;
-  // height: 572.6px;
   margin-top: 29%;
   flex-grow: 1;
   position: relative;
@@ -151,7 +190,6 @@ export default {
     position: absolute;
     left: 0;
     top: 0;
-    color: #FFFFFF;
     margin: 13.5% 2.7%;
   }
   &__desc {
@@ -162,15 +200,24 @@ export default {
       font-size: 22px;
       line-height: 130%;
       color: #FFFFFF;
-      width: 665px;
+      width: 63.4%;
     }
+  }
+  p.home__mid-titles {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 120%;
+    letter-spacing: 0.15em;
+    text-transform: uppercase;
+    color: #FFFFFF;
   }
   &__job-overview {
     width: 1032.68px;
     padding: 18.39px 0;
     max-width: calc(100% - 5.4%);
     border-top: 1px solid #707070;
-    &:last-of-type {
+    &--last {
       border-bottom: 1px solid #707070;
     }
     &-left {
@@ -241,6 +288,134 @@ export default {
 
         &:last-of-type {
           margin-bottom: 0;
+        }
+      }
+    }
+  }
+  &__slider {
+    width: 1032.69px;
+    height: 382px;
+    max-width: calc(100% - 5.4%);
+    background: #1B2626;
+    position: relative;
+    margin-bottom: 82px;
+    &-text {
+      width: 46.7%;
+      margin-top: 42.66px;
+      margin-left: 17.7%;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 17px;
+      line-height: 150%;
+      color: #DDDFDF;
+    }
+    &-who {
+      width: 46.7%;
+      margin-top: 10px;
+      margin-left: 17.7%;
+      // margin-left: 21.2%;
+      > div {
+        width: 24.5px;
+        height: 14px;
+        margin-right: 7px;
+        border-bottom: 1px solid #000000;
+      }
+      p {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 17px;
+        line-height: 150%;
+        color: #DDDFDF;
+        span {
+          font-style: normal;
+          font-weight: 700;
+          font-size: 17px;
+          line-height: 130%;
+          color: #FFFFFF;
+        }
+      }
+    }
+    &-image {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+    }
+    &-apostrophe {
+      position: absolute;
+      width: 60%;
+      top: 48px;
+      left: 0;
+      margin-left: 10.5%;
+    }
+    &-pointers {
+      position: absolute;
+      width: 1105px;
+      top: 50%;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    &-circles {
+      position: absolute;
+      bottom: -20px;
+      left: 0;
+    }
+  }
+  &__bluebox {
+    width: 100vw;
+    height: 472.7px;
+    position: relative;
+    &-bg {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(108.05deg, #00C9A5 -1.97%, #000F59 119.17%);
+      transform: matrix(1, 0, 0, -1, 0, 0);
+    }
+    &-container {
+      width: 1032.69px;
+      margin-top: 38.84px;
+      max-width: calc(100% - 5.4%);
+    }
+    &-container-2 {
+      width: 661.64px;
+      gap: 15px;
+      * {
+        z-index: 1;
+      }
+      p {
+        font-size: 17px;
+        line-height: 150%;
+        color: #FFFFFF;
+      }
+    }
+    &-divider {
+      width: 124px;
+      height: 0px;
+      border-bottom: 1px solid #34eac9;
+    }
+    &-button {
+      width: 246px;
+      height: 48px;
+      background: #FFFFFF;
+      border: 1px solid #FFFFFF;
+      border-radius: 24px;
+      transition: all 0.2s;
+      span {
+        font-family: 'Gotham_KTR--bold';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 12px;
+        line-height: 100%;
+        text-align: center;
+        letter-spacing: 0.15em;
+        text-transform: uppercase;
+        color: #1B2626;
+        transition: all 0.2s;
+      }
+      &:hover {
+        background: transparent;
+        span {
+          color: #FFFFFF;
         }
       }
     }
